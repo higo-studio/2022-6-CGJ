2318b76a662c88684d3ed0a52492403451d6d9f8
mooooooi <wing.mo@coconutislandstudio.com>
2022-06-25 11:28:45 +0800

    chore: 添加MyBox

---
+++ Assets/Scenes/MovableManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyBox;
+
+public class MovableManager : MonoBehaviour
+{
+    [ReadOnly]
+    public GameObject[] Movables;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
