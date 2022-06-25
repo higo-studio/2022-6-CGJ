b17bd89c3de427df455ee2d84d11a170dfe7a501
mooooooi <wing.mo@coconutislandstudio.com>
2022-06-25 15:57:54 +0800

    ﻿feat: 交换动画及编辑器工具

---
+++ Assets/Scenes/Editor/MovableManagerEditor.cs
@@ -0,0 +1,145 @@
+using UnityEngine;
+using UnityEditor;
+using DG.DOTweenEditor;
+using DG.Tweening;
+using EGUIL = UnityEditor.EditorGUILayout;
+using GUIL = UnityEngine.GUILayout;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Core.PathCore;
+using System.Reflection;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Rendering;
+
+[CustomEditor(typeof(MovableManager))]
+public class MovableManagerEditor : Editor
+{
+    private int aIdx;
+    private int bIdx;
+
+    private Vector3 rawAPos;
+    private Vector3 rawBPos;
+
+    private MovableManager t;
+    private GUIStyle labelStyle;
+
+    private void Awake()
+    {
+    }
+    private void OnEnable()
+    {
+        t = (MovableManager)target;
+        bIdx = t.Movables.Count - 1;
+        rawAPos = t.Movables[aIdx].position;
+        rawBPos = t.Movables[bIdx].position;
+        Debugger.SetLogPriority(LogBehaviour.Verbose);
+    }
+
+    private void OnDisable()
+    {
+        if (DOTweenEditorPreview.isPreviewing)
+        {
+            StopPreview();
+        }
+    }
+    public override void OnInspectorGUI()
+    {
+        var needRedraw = false;
+        var needRefreshPos = false;
+
+        var preEnabled = GUI.enabled;
+        GUI.enabled = !DOTweenEditorPreview.isPreviewing;
+        DrawDefaultInspector();
+        if (GUIL.Button("Collect hats"))
+        {
+            t.CollectHats();
+            needRefreshPos = true;
+        }
+        if (GUIL.Button("Settleup hats"))
+        {
+            t.SettleupHats();
+            needRefreshPos = true;
+        }
+        var max = t.Movables.Count - 1;
+
+        EditorGUI.BeginChangeCheck();
+        aIdx = EGUIL.IntSlider("Controll Point A", aIdx, 0, max);
+        if (EditorGUI.EndChangeCheck() || needRefreshPos)
+        {
+            rawAPos = t.Movables[aIdx].position;
+            needRedraw = true;
+        }
+
+        EditorGUI.BeginChangeCheck();
+        bIdx = EGUIL.IntSlider("Controll Point B", bIdx, 0, max);
+        if (EditorGUI.EndChangeCheck() || needRefreshPos)
+        {
+            rawBPos = t.Movables[bIdx].position;
+            needRedraw = true;
+        }
+
+        GUI.enabled = preEnabled;
+        var btnStr = DOTweenEditorPreview.isPreviewing ? "Stop Preview Path" : "Start Preview Path";
+        if (GUIL.Button(btnStr))
+        {
+            if (!DOTweenEditorPreview.isPreviewing)
+            {
+                var (ta, tb) = t.CreateSwapAnimation(aIdx, bIdx);
+                var tween = t.Combine(ta, tb);
+                DOTweenEditorPreview.PrepareTweenForPreview(tween);
+                DOTweenEditorPreview.Start();
+            }
+            else
+            {
+                StopPreview();
+            }
+        }
+        if (needRedraw || needRefreshPos)
+        {
+            EditorWindow view = EditorWindow.GetWindow<SceneView>();
+            view.Repaint();
+        }
+
+    }
+
+    void StopPreview()
+    {
+        DOTweenEditorPreview.Stop(true, true);
+    }
+
+    private void OnSceneGUI()
+    {
+
+        if (labelStyle == null)
+        {
+            labelStyle = new GUIStyle();
+            labelStyle.normal.textColor = Color.red;
+            labelStyle.fontSize = 100;
+        }
+
+        var worldAPos = t.APoint.Get(rawAPos, rawBPos);
+        Handles.Label(worldAPos, "A", labelStyle);
+        EditorGUI.BeginChangeCheck();
+        var newWorldAPos = Handles.PositionHandle(worldAPos, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck())
+        {
+            t.APoint.Set(newWorldAPos, rawAPos, rawBPos);
+        }
+
+        var worldBPos = t.BPoint.Get(rawBPos, rawAPos);
+        Handles.Label(worldBPos, "B", labelStyle);
+        EditorGUI.BeginChangeCheck();
+        var newWorldBPos = Handles.PositionHandle(worldBPos, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck())
+        {
+            t.BPoint.Set(newWorldBPos, rawBPos, rawAPos);
+        }
+
+        var preZTest = Handles.zTest;
+        Handles.zTest = CompareFunction.LessEqual;
+        Handles.DrawBezier(rawAPos, rawBPos, t.APoint.Get(rawAPos, rawBPos), t.BPoint.Get(rawBPos, rawAPos), Color.green, null, 2f);
+        Handles.DrawBezier(rawBPos, rawAPos, t.APoint.Get(rawBPos, rawAPos), t.BPoint.Get(rawAPos, rawBPos), Color.red, null, 2f);
+        Handles.zTest = preZTest;
+    }
+}
--- Assets/Scenes/MovableManager.cs
+++ Assets/Scenes/MovableManager.cs
@@ -1,21 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using MyBox;
+using DG.Tweening;
+using DG.Tweening.Core;
+
+[Serializable]
+public class RelativePoint
+{
+    [SerializeField]
+    private Vector3 value;
+
+    public void Set(Vector3 point, Vector3 start, Vector3 end)
+    {
+        var dir = (end - start).normalized;
+        var root = Matrix4x4.TRS(start, Quaternion.LookRotation(dir), Vector3.one);
+        value = root.inverse.MultiplyPoint(point);
+    }
+
+    public Vector3 Get(Vector3 start, Vector3 end)
+    {
+        var dir = (end - start).normalized;
+        var root = Matrix4x4.TRS(start, Quaternion.LookRotation(dir), Vector3.one);
+        return root.MultiplyPoint(value);
+    }
+}
 
 public class MovableManager : MonoBehaviour
 {
+    [Range(0.5f, 5)]
+    public float Duration = 2f;
+    public Transform Container;
+    public RelativePoint APoint;
+    public RelativePoint BPoint;
+    [Header("Ô¤ÀÀÇø")]
     [ReadOnly]
-    public GameObject[] Movables;
+    public List<Transform> Movables;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CollectHats();
+    }
+
+    public void CollectHats()
+    {
+        Movables.Clear();
+        foreach (Transform trs in Container)
+        {
+            Movables.Add(trs);
+        }
+    }
+
+    public void SettleupHats()
+    {
+        var center = Movables.Count / 2f;
+        for (var i = 0; i < Movables.Count; i++)
+        {
+            Movables[i].position = new Vector3((i - center) * 4, 0, 0);
+        }
+    }
+
+    public (Tween a, Tween b) CreateSwapAnimation(int aIdx, int bIdx)
+    {
+        var a = Movables[aIdx];
+        var b = Movables[bIdx];
+        var aT = a.DOPath(
+            new Vector3[] { b.transform.position, APoint.Get(a.position, b.position), BPoint.Get(b.position, a.position) }
+            , Duration, PathType.CubicBezier, PathMode.Sidescroller2D);
+        var bT = b.DOPath(
+            new Vector3[] { a.transform.position, APoint.Get(b.position, a.position), BPoint.Get(a.position, b.position) }
+            , Duration, PathType.CubicBezier, PathMode.Sidescroller2D);
+
+        //aT.PathGetDrawPoints
+        return (aT, bT);
+    }
+
+    public Tween Combine(Tween ta, Tween tb)
+    {
+        var seq = DOTween.Sequence();
+        seq.Join(ta).Join(tb);
+        return seq;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
