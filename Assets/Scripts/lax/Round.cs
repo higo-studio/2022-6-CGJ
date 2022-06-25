d3c6da7907327287ea64b7ca104417cec6d3aafb
Mr-lax <757572189@qq.com>
2022-06-25 19:41:39 +0800

    关卡管理

---
+++ Assets/Scripts/lax/Round.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Round
+{
+    // 球的颜色ID
+    [SerializeField]
+    public int ballColor;
+
+    // 帽子的颜色ID
+    [SerializeField]
+    public int hatColor;
+
+    // 帽子的转速
+    [SerializeField]
+    public int hatSpeed;
+
+    // 帽子转的次数
+    [SerializeField]
+    public int changeNum;
+
+    // 帽子的个数
+    [SerializeField]
+    public int hatNum;
+
+    // 结局类型：选小球还是输入,1为选小球，2为输入
+    [SerializeField]
+    public string endType;
+}
